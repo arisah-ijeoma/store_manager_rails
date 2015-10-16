8f39923d968188813306d2f1e84fb9483f25d64f
Jay <gift_ij2002@yahoo.com>
2015-10-16 09:59:25 +0100

    log in option shows after error

--- spec/features/devise_spec.rb
+++ spec/features/devise_spec.rb
@@ -10,4 +10,22 @@ describe "Devise", type: :feature do
     visit new_user_session_path
     expect(page).to have_content('Log in as Admin')
   end
+
+  scenario "admin login error sill shows user option" do
+    visit new_admin_user_session_path
+    fill_details
+    expect(page).to have_content('Log in as User')
+  end
+
+  scenario "user login error sill shows admin option" do
+    visit new_user_session_path
+    fill_details
+    expect(page).to have_content('Log in as Admin')
+  end
+
+  def fill_details
+    fill_in 'Email', with: 'x.com'
+    fill_in 'Password', with: 'x.comske'
+    click_on 'Log in'
+  end
 end
