a7e2c4adb22cff37608f708e02519d926db3344e
Jay <gift_ij2002@yahoo.com>
2015-10-20 16:37:23 +0100

    wip - super admin can create regular admin
    [#103063614]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions', omniauth_callbacks: "omniauth_callbacks" }
-  devise_for :admin_users, :skip => [:registrations] , controllers: { sessions: "admin/sessions" }
+  devise_for :admin_users, skip: :registrations , controllers: { sessions: "admin/sessions" }
 
   namespace :admin do
     root 'items#index'
