d01138c3172a83f13c357348da0dfd2825ce0b1c
Jay <gift_ij2002@yahoo.com>
2015-10-24 21:37:52 +0100

    fill failing specs
    [#105752508]

--- spec/features/devise_spec.rb
+++ spec/features/devise_spec.rb
@@ -9,7 +9,7 @@ describe "Devise", type: :feature do
 
     scenario "user login error sill shows admin option" do
       visit new_user_session_path
-      fill_details
+      fill_user_details
       expect(page).to have_content('Log in as Admin')
     end
   end
@@ -22,7 +22,7 @@ describe "Devise", type: :feature do
 
     scenario "admin login error sill shows user option" do
       visit new_admin_user_session_path
-      fill_details
+      fill_admin_details
       expect(page).to have_content('Log in as User')
     end
 
@@ -33,9 +33,15 @@ describe "Devise", type: :feature do
     end
   end
 
-  def fill_details
-    fill_in 'Email', with: 'x.com'
-    fill_in 'Password', with: 'x.comske'
+  def fill_user_details
+    fill_in 'user_email', with: 'x.com'
+    fill_in 'user_password', with: 'x.comske'
+    click_on 'Log in'
+  end
+
+  def fill_admin_details
+    fill_in 'admin_user_email', with: 'x.com'
+    fill_in 'admin_user_password', with: 'x.comske'
     click_on 'Log in'
   end
 end
