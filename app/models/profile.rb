9bd568ff096395aba8706ed3767b334a42421ffe
Jay <gift_ij2002@yahoo.com>
2015-11-02 09:51:18 +0100

    allow blank for phone number

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,7 @@ class Profile < ActiveRecord::Base
   accepts_nested_attributes_for :admin_user
 
   validates :mobile_number, presence: true, uniqueness: true, length: { is: 11 }, on: :update
-  validates :phone_number, uniqueness: { length: { is: 11 } }, on: :update
+  validates :phone_number, allow_blank: true, uniqueness: { length: { is: 11 } }, on: :update
 
   delegate :first_name, :last_name, :establishment, :role, to: :admin_user, allow_nil: true
   delegate :first_name, :last_name, :nick, to: :user, allow_nil: true
