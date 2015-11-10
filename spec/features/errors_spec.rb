bae6d66074d3c3c7d5bbe0507be3917c8c37bfce
Jay <gift_ij2002@yahoo.com>
2015-11-10 11:15:37 +0100

    handle 404 error
    [#107693780]

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 describe "Error Page", type: :feature do
-  let(:user) { create(:user) }
+  context "Employee" do
+    let(:user) { create(:user) }
 
-  scenario "app does not raise an error" do
-    visit root_path
-    login user
+    scenario "app does not raise an error" do
+      visit root_path
+      login user
 
-    visit '/items/new'
-    expect(page).to have_content('Return to app')
+      visit '/items/new'
+      expect(page).to have_content('Return to app')
+    end
+  end
+
+  context "Admin" do
+    let(:admin_user) { create(:admin_user) }
+
+    scenario "invalid record" do
+      visit admin_root_path
+      admin_login admin_user
+      visit "admin/items/50/edit"
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
   end
 end
